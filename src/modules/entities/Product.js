0b79480748beab9d77bfcb06fc618c98b3bd9b50
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-09-22 06:10:50 +0200

    Added format price method

--- src/modules/entities/Product.js
+++ src/modules/entities/Product.js
@@ -9,4 +9,13 @@ export class Product {
         this.url = data.url;
         this.imageUrl = data.image_url;
     }
+
+    formatPrice(locale) {
+        const major = this.price / 100;
+        try {
+            return new Intl.NumberFormat(locale, { style: 'currency', currency: this.currency });
+        } catch {
+            return `${major.toFixed(2)} ${this.currency}`;
+        }
+    }
 }
