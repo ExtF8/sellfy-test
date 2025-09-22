93e9dffe243bddbac8e3d3f81f9bcade400a3f9e
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-09-22 07:31:26 +0200

    Added AppController class

--- index.js
+++ index.js
@@ -1,8 +1,63 @@
 import ProductServiceApi from './src/modules/services/ProductApiService';
+import ProductListView from './src/modules/views/ProductListView';
 
-const API_URL =
-    'https://raw.githubusercontent.com/Sellfy/test-assignment-frontend/refs/heads/master/products.json';
+// App controller
+class AppController {
+    constructor() {
+        const API_URL =
+            'https://raw.githubusercontent.com/Sellfy/test-assignment-frontend/refs/heads/master/products.json';
+        const app = document.getElementById('app');
+        this.productItems = new ProductServiceApi(API_URL);
+        this.products = [];
+        (this.view = new ProductListView(app)),
+            {
+                onShare: product => this.shareProduct(product),
+                onDelete: product => this.deleteProduct(product.id),
+            };
+    }
 
-const productItems = new ProductServiceApi(API_URL);
+    // Initialize app
+    async init() {
+        this.view.showLoading();
+        try {
+            this.products = await this.productItems.getAll();
+            this.view.render(this.products);
+        } catch (error) {
+            this.view.showError(error.message || 'Failed to load products', () => this.init());
+        }
+    }
 
-console.log(productItems);
+    // Delete product
+    deleteProduct(id) {
+        this.products = this.products.filter(product => product.id !== id);
+        this.view.render(this.products);
+    }
+
+    // Share product
+    async shareProduct(product) {
+        const url = product.url;
+        const text = `${product.name} - ${product.decription}`;
+
+        if (navigator.share) {
+            try {
+                await navigator.share({
+                    title: product.name,
+                    text,
+                    url,
+                });
+                return;
+            } catch (error) {
+                console.log('Error sharing product: ', error);
+            }
+        }
+        try {
+            await navigator.clipboard.writeText(url);
+            alert('Link copied');
+        } catch {
+            alert(url);
+        }
+    }
+}
+
+// Init app
+new AppController().init;
